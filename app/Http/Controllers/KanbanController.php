6be1767cd5b9019fd42c8537a05bc4fb7ea1ee97
AmmarJ23 <ammar.jmldn@outlook.com>
2023-12-11 14:43:45 +0800

    Read and display tasks by project_id and sprint_id

--- app/Http/Controllers/KanbanController.php
+++ app/Http/Controllers/KanbanController.php
@@ -10,8 +10,8 @@ class KanbanController extends Controller
 {
     public function kanbanIndex($proj_id, $sprint_id)
     {
-        $statuses = Status::where('project_id', "2")->get();
-        $tasks = Task::where("proj_id", "2")->get();
+        $statuses = Status::where('project_id', $proj_id)->get();
+        $tasks = Task::where("proj_id", $proj_id)->where("sprint_id", $sprint_id)->get();
 
         // Group tasks by status id
         $tasksByStatus = [];
