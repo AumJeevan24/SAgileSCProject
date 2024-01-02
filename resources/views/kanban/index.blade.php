c3ca0f102227965e5ece393bc8e6cfb76a24647d
AmmarJ23 <ammar.jmldn@outlook.com>
2024-01-03 00:29:10 +0800

    Edit kanban css

--- resources/views/kanban/index.blade.php
+++ resources/views/kanban/index.blade.php
@@ -8,6 +8,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <title>Kanban Board for {{ $sprint->sprint_name }}</title>
 
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;700&display=swap">
     <link rel="stylesheet" href="{{ asset('styles.css') }}" />
 
 </head>
