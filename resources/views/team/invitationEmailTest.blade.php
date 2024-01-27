7aeb8c6425aea37f1d1b42c90b518828a992a4eb
amarulakmal <amarulakmal@yahoo.com>
2024-01-27 09:26:59 +0800

    email message update

--- resources/views/team/invitationEmailTest.blade.php
+++ resources/views/team/invitationEmailTest.blade.php
@@ -1,9 +1,11 @@
 @component('mail::message')
 
-You are invite to join our team. please click the link below to join our team.
+Admins[team leader] has invited you to collaborate on this team project. 
+you can see your team members by click View invitation
+{{-- You are invite to join our team. please click the link below to join our team. --}}
 
 @component('mail::button',['url' => 'http://127.0.0.1:8000/home'])
-   Join Now
+   View invitation 
 @endcomponent
 
 @endcomponent
