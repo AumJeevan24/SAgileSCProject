05b7f8a86f0f07e9f028596d6b09cb95a50c3e96
AmmarJ23 <ammar.jmldn@outlook.com>
2024-01-30 19:01:29 +0800

    Delete Unused Code
    Theme Related Files

--- app/Http/Controllers/ThemeController.php
+++ app/Http/Controllers/ThemeController.php
@@ -9,24 +9,11 @@ use Illuminate\Http\Request;
 
 class ThemeController extends Controller
 {
-    public function chooseTheme(ThemeConfig $themeConfig)
-    {
-        $availableThemes = ['theme1', 'theme2', 'theme3'];
-        return view('theme.choose', ['themes' => $availableThemes, 'currentTheme' => $themeConfig->getTheme()]);
-    }
-
     public function setTheme(ThemeConfig $themeConfig, Request $request)
     {
         $selectedTheme = $request->input('theme');
         $themeConfig->setTheme($selectedTheme);
         return redirect()->back();
-    }
-
-    public function showTheme(ThemeConfig $themeConfig)
-    {
-        $theme = $themeConfig->getTheme();
-        $styleFile = $themeConfig->getThemeCssFile();
-        return view('theme.show', ['theme' => $theme, 'styleFile' => $styleFile]);
     }
 }
 
